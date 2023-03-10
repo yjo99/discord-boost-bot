8ba98aedf53767c206257eb6e4bcf0b57925b5ce
Yousef_Sayed <yousef16992@gmail.com>
2023-03-10 16:26:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord, os, json, hashlib
 from boosting import *
-from pycuolor import color
+from pycoloring import color
 from auto import *
 if os.name == 'nt':
     import ctypes
